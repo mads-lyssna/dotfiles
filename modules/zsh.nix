d92f2a601c9b1f8eabd355ae4b8908d5880275bd
Madi Ostoja <madi@madeleineostoja.com>
2026-05-14 10:13:58 +1200

    add git fetch and cherry-pick aliases

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -51,7 +51,9 @@
       gpushf = "git push --force-with-lease --force-if-includes";
       greset = "git reset HEAD~1";
       grecommit = "git commit --amend -C HEAD";
+      gcp = "git cherry-pick";
       gr = "git rebase";
+      gf = "git fetch";
       lg = "lazygit";
 
       # Shortcuts
