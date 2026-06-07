582a297ae0c83c9b14d241269f745ec323562a6e
Madi Ostoja <madi@madeleineostoja.com>
2026-06-07 15:28:57 +1200

    feat: move to unstable nix channels

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     agents = {
