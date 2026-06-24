b0e56f7b170d3a67c2e02ab7dcee143f0fb8efd3
Madi Ostoja <madeleine.ostoja@lyssna.com>
2026-06-24 13:12:42 +1200

    fix pnpm pat

--- home.nix
+++ home.nix
@@ -92,7 +92,7 @@ in
   # Ensure global pnpm packages are installed
   home.activation.pnpmGlobals = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     export PNPM_HOME="${pnpmHome}"
-    export PATH="${pkgs.nodejs}/bin:${pkgs.pnpm}/bin:$PNPM_HOME/bin:$PATH"
+    export PATH="${pkgs.nodejs}/bin:${pkgs.pnpm}/bin:$PNPM_HOME:$PNPM_HOME/bin:$PATH"
     for pkg in ${lib.escapeShellArgs pnpmGlobals}; do
       if ! ${pkgs.pnpm}/bin/pnpm ls -g --depth=0 2>/dev/null | grep -q "$pkg"; then
         run ${pkgs.pnpm}/bin/pnpm add -g --ignore-scripts "$pkg"
